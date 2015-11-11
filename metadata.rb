9917711dd9dd0a44f17b625dd02cadbdac636ca3
Maurizio Pillitu <maoo@apache.org>
2015-11-11 10:26:56 +0100

    Update metadata.rb
    another fix to cookbook version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@chef.io'
 license          'Apache 2.0'
 description      'Installs/Configures tomcat'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.17.3.fork8'
+version          '0.17.3.8'
 
 depends 'java'
 depends 'line'
